ff1c11432113691ea7c1c1a7e88168cef9f774ae
Thiha Kyaw <thihakyaw.dev@gmail.com>
2022-10-08 01:16:45 +0700

    unit test

--- src/lists/lists.resolver.spec.ts
+++ src/lists/lists.resolver.spec.ts
@@ -1,88 +1,72 @@
-import { INestApplication } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Test } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from '../tasks/tasks.module';
-import { Repository } from 'typeorm';
-import { List } from './entities/list.entity';
-import { ListsModule } from './lists.module';
-import DatabaseConfig from '../../config/database-config';
-import * as request from 'supertest'
+import { Test, TestingModule } from '@nestjs/testing';
+import { ListsService } from './lists.service';
+import { ListsResolver } from './lists.resolver';
 
-describe('ListsResolver', () => {
-  let app: INestApplication;
-  let repository: Repository<List>;
+describe('ListResolver', () => {
+  let resolver: ListsResolver;
 
-  beforeAll(async () => {
-    const module = await Test.createTestingModule({
-      imports: [
-        ListsModule,
-        TasksModule,
-        ConfigModule.forRoot({
-          isGlobal: true,
-          load: [
-            DatabaseConfig, 
-          ],
-        }),
-        TypeOrmModule.forRootAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: (configService: ConfigService) => ({
-            type: 'mysql',
-            host: configService.get<string>('database.host'),
-            port: configService.get<number>('database.port'),
-            username: configService.get<string>('database.username'),
-            password: configService.get<string>('database.password'),
-            database: configService.get<string>('database.test_database'),
-            entities: ['dist/**/*.entity{.ts,.js}'],
-            synchronize: true
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ListsResolver,
+        {
+          provide: ListsService,
+          useFactory: () => ({
+            findAll: jest.fn(() => [
+              {
+                id: 1,
+                name: 'Product Meeting',
+              },
+              {
+                id: 2,
+                name: 'Sprint Planning',
+              },
+              {
+                id: 3,
+                name: 'Backend List',
+              },
+            ]),
+            findOne: jest.fn(() => [
+              {
+                id: 1,
+                name: 'Product Meeting',
+              }
+            ]),
           }),
-        }),
+        },
       ],
     }).compile();
-    app = module.createNestApplication();
-    await app.init();
-    repository = module.get('ListRepository');
-  });
-  
-  afterEach(async () => {
-    //await repository.query(`DELETE FROM lists;`);
+
+    resolver = module.get<ListsResolver>(ListsResolver);
   });
 
-  afterAll(async () => {
-    await app.close();
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
   });
 
-  describe('findAll', () => {
-
-    it('should return an array of lists', async () => {
-      // await repository.save([
-      //   { name: 'Test List #1' },
-      //   { name: 'Test List #2' }
-      // ]);
-      const mutation = `
+  describe('lists', () => {
+    it('should get the list array', async () => {
+      expect(await resolver.findAll()).toEqual([
         {
-           lists {
-             name
-           }
-        }
-      `
+          id: expect.any(Number),
+          name: 'Product Meeting',
+        },
+        {
+          id: expect.any(Number),
+          name: 'Sprint Planning',
+        },
+        {
+          id: expect.any(Number),
+          name: 'Backend List',
+        },
+      ]);
+    });
 
-      const { body } = await request(app.getHttpServer())
-        .post('/graphql')
-        .send({
-          query: mutation,
-        })
-
-      expect(body).toEqual({
-        data: {
-          lists: [
-            { name: 'Test List #1' },
-            { name: 'Test List #2' }
-          ]
-        }
-      });
+    it('should get one list', async () => {
+      expect(await resolver.findOne(1)).toEqual([{
+          id: expect.any(Number),
+          name: 'Product Meeting',
+        }]);
     });
   });
-
 });
