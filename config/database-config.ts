cba744641a6958ddb8a7892a0714e153d85e0356
Thiha Kyaw <thihakyaw.dev@gmail.com>
2022-10-08 11:07:30 +0700

    update .env.example

--- config/database-config.ts
+++ config/database-config.ts
@@ -3,7 +3,6 @@ export default () => ({
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10) || 3306,
     database: process.env.DB_DATABASE,
-    test_database: process.env.DB_DATABASE_TEST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
   },
